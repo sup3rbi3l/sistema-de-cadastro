7c77fc37c378d58bd9e6cbc6232af5e7e7c5bf18
sup3rbi3l <sup3rgabri3l@gmail.com>
2023-07-01 10:47:39 -0300

    fazendo o l

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("socorro  eu quero me matar")
 console.log("acho que eu consegui asgdyuasdrt6yasdf drt6y")
+console.log("faraiz o l?")
